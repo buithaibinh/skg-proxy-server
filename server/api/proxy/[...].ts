134675bb8c5e3c9cd7829b613462eec56be9b751
Binh Bui <binhbv@gmail.com>
2022-12-02 12:22:17 +0700

    headerssss

--- server/api/proxy/[...].ts
+++ server/api/proxy/[...].ts
@@ -10,9 +10,19 @@ export default defineEventHandler(async (event) => {
     res.end('url is required');
   }
 
+  console.log('headers', req.method);
+
   // proxy route
-  const proxyRes = await fetch(url, {
-    // proxy request headers
+  const urlSearchParams = new URLSearchParams(req.url?.split('?')[1]);
+  const finalUrl = new URL(url);
+  urlSearchParams.forEach((value, key) => {
+    finalUrl.searchParams.append(key, value);
+  });
+
+  const headers: any = { ...req.headers };
+  const proxyRes = await fetch(finalUrl, {
+    method: req.method,
+    headers
   });
   const body = await proxyRes.text();
   res.end(body);
