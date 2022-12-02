de07b5a76b29803f6e364b6c99ca3fc907a4f5ac
Binh Bui <binhbv@gmail.com>
2022-12-02 15:06:03 +0700

    aaaa

--- server/api/proxy/[...].ts
+++ server/api/proxy/[...].ts
@@ -16,33 +16,37 @@ export default defineEventHandler(async (event) => {
   const { req, res, context } = event;
 
   // request params
-  const url = context.params._;
+  // const url = context.params._;
 
-  // validate url
-  if (!url || !url.startsWith('http')) {
-    res.statusCode = 400;
-    res.end('url is required');
-  }
+  // // validate url
+  // if (!url || !url.startsWith('http')) {
+  //   res.statusCode = 400;
+  //   res.end('url is required');
+  // }
 
-  console.log('headers', req.method);
+  // console.log('headers', req.method);
 
-  // proxy route
-  const urlSearchParams = new URLSearchParams(req.url?.split('?')[1]);
-  const finalUrl = new URL(url);
-  urlSearchParams.forEach((value, key) => {
-    finalUrl.searchParams.append(key, value);
-  });
+  // // proxy route
+  // const urlSearchParams = new URLSearchParams(req.url?.split('?')[1]);
+  // const finalUrl = new URL(url);
+  // urlSearchParams.forEach((value, key) => {
+  //   finalUrl.searchParams.append(key, value);
+  // });
 
-  const headers: any = { ...req.headers };
+  // const headers: any = { ...req.headers };
 
-  const proxyRes = await fetch(finalUrl, {
-    method: req.method
-    // headers
-  });
+  // const proxyRes = await fetch(finalUrl, {
+  //   method: req.method
+  //   // headers
+  // });
 
-  // set cors policy
-  // setCORSPolicy(res);
+  // // set cors policy
+  // // setCORSPolicy(res);
 
-  const body = await proxyRes.text();
-  res.end(body);
+  // const body = await proxyRes.text();
+  // res.end(body);
+
+  return {
+    status: 'success'
+  };
 });
