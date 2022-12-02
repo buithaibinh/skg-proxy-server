ec64583247b4e24649e88e061d4b5460009a3742
Binh Bui <binhbv@gmail.com>
2022-12-02 16:35:41 +0700

    encode url

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
   <h1>Proxy server</h1>
+  <button @click="fetchData">Fetch data</button>
 </template>
 
-<script setup></script>
+<script setup>
+const fetchData = async () => {
+  // https://www.city.shinjuku.lg.jp/top_rss.rdf
+  const baseUrl = 'https://www.city.shinjuku.lg.jp/top_rss.rdf?xx==11'
+  const response = await fetch('http://localhost:3000/api/proxy/' + encodeURIComponent(baseUrl));
+  const data = await response.text();
+  console.log(data);
+};
+</script>
--- server/api/proxy/[...].ts
+++ server/api/proxy/[...].ts
@@ -2,7 +2,10 @@ export default defineEventHandler(async (event) => {
   const { req, res, context } = event;
 
   // request params
-  const url = context.params._;
+  let url = context.params._ || '';
+
+  url = decodeURIComponent(url);
+  console.log('url', url);
 
   // validate url
   if (!url || !url.startsWith('http')) {
@@ -11,7 +14,7 @@ export default defineEventHandler(async (event) => {
   }
 
   // proxy route
-  const urlSearchParams = new URLSearchParams(req.url?.split('?')[1]);
+  const urlSearchParams = new URLSearchParams(url?.split('?')[1]);
   const finalUrl = new URL(url);
   urlSearchParams.forEach((value, key) => {
     finalUrl.searchParams.append(key, value);
